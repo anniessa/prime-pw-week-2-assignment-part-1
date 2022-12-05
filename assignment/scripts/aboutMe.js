f8412771601acca33d7d3f8566b907f73b63d753
Anniessa Antar <anniessa@bousayha.hsd1.mn.comcast.net>
2022-12-05 12:55:12 -0600

    first name and last name variables set

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,7 +1,12 @@
+console.log('js');
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
+let firstName = 'Anniessa';
+console.log(firstName);
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+let lastName = 'Antar';
+console.log(lastName);
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
